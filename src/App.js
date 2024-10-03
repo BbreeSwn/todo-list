394aa615dab26cc549c55f74303eaa57c6444935
BbreeSwn <suwanan.cs@hotmail.com>
2024-10-03 13:04:53 +0700

    feat: create style

--- src/App.js
+++ src/App.js
@@ -5,8 +5,11 @@ function App() {
     <section className="container">
       <h1>Todo List</h1>
       <form className='form-group'>
+        <div className='form-control'>
         <input type="text" className='text-input' />
         <button type='submit' className='submit-btn'>add list</button>
+
+        </div>
       </form>
     </section>
   );
