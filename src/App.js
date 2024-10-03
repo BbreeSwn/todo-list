662bf045d046b53b6aab4facf170f3a1268ad33b
BbreeSwn <suwanan.cs@hotmail.com>
2024-10-03 15:26:19 +0700

    feat: uuid set newItem

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
-import './App.css';
-import {useState} from 'react'
+import "./App.css";
+import { useState } from "react";
+import { v4 as uuidv4 } from "uuid";
 
 function App() {
-  const [name , setName] = useState('')
+  const [name, setName] = useState("");
+  const [list, setList] = useState([]);
 
-const submitData = (e) => {
-e.preventDefault()
-console.log("data =" ,name);
-}
+  const submitData = (e) => {
+    e.preventDefault();
+    const newItem = {
+      id: uuidv4(),
+      title: name,
+    };
+    setList([...list,newItem]);
+    setName('')
+  };
 
   return (
     <section className="container">
       <h1>Todo List</h1>
-      <form className='form-group' onSubmit={submitData} >
-        <div className='form-control'>
-        <input type="text" className='text-input' onChange={(e)=>setName(e.target.value)}
-        value={name}
-        />
-        <button type='submit' className='submit-btn'>add list</button>
-
+      <form className="form-group" onSubmit={submitData}>
+        <div className="form-control">
+          <input
+            type="text"
+            className="text-input"
+            onChange={(e) => setName(e.target.value)}
+            value={name}
+          />
+          <button type="submit" className="submit-btn">
+            add list
+          </button>
         </div>
       </form>
     </section>
