b83e0a4e368814d35c645d289578ea8a56b5a276
BbreeSwn <suwanan.cs@hotmail.com>
2024-10-03 14:57:05 +0700

    feat: set state

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import './App.css';
+import {useState} from 'react'
 
 function App() {
+  const [name , setName] = useState('')
+
+const submitData = (e) => {
+e.preventDefault()
+console.log("data =" ,name);
+}
+
   return (
     <section className="container">
       <h1>Todo List</h1>
-      <form className='form-group'>
+      <form className='form-group' onSubmit={submitData} >
         <div className='form-control'>
-        <input type="text" className='text-input' />
+        <input type="text" className='text-input' onChange={(e)=>setName(e.target.value)}
+        value={name}
+        />
         <button type='submit' className='submit-btn'>add list</button>
 
         </div>
